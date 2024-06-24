9355d2456fbf70e88003a9779c98e268af5dc6ae
Lokesh Bansal <lokesh@75way.com>
2024-06-24 17:51:04 +0530

    schema code for secondary email or employee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_104848) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_042214) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -98,6 +98,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_104848) do
     t.integer "companies_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "secondary_email"
     t.index ["companies_id"], name: "index_employees_on_companies_id"
     t.index ["email"], name: "index_employees_on_email", unique: true
   end
